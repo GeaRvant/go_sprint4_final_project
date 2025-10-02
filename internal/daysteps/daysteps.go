507bcb945db00f51d81c02d63a80cfa6f3b23171
Anatoly Vrublevskikh <vrublevskikh.anatolij@yandex.ru>
2025-10-02 22:08:07 +0300

    Functions implemented

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,9 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -12,9 +15,40 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	strParts := strings.Split(data, ",")
+	if len(strParts) != 2 {
+		return 0, 0, fmt.Errorf("Ошибка преобразования строки: ожидалось 2 части, получено %d", len(strParts))
+	}
+	elementToInt, err := strconv.Atoi(strings.TrimSpace(strParts[0]))
+	if err != nil {
+		return 0, 0, fmt.Errorf("Ошибка преобразования в int: %w", err)
+	}
+	if elementToInt < 0 {
+		return 0, 0, fmt.Errorf("Ошибка: отрицательное количество шагов (%d)", elementToInt)
+	}
+	duration, err := time.ParseDuration(strings.TrimSpace(strParts[1]))
+	if err != nil {
+		return 0, 0, fmt.Errorf("Ошибка парсинга длительности: %w", err)
+	}
+	return elementToInt, duration, nil
+
 }
 
 func DayActionInfo(data string, weight, height float64) string {
 	// TODO: реализовать функцию
+	walkSteps, walkDuration, err := parsePackage(data)
+	if err != nil {
+		return fmt.Sprintf("Ошибка при получении данных: %v", err)
+	}
+	if walkSteps < 0 {
+		return fmt.Sprintf("Ошибка: отрицательное количество шагов (%d)", walkSteps)
+	}
+	if walkDuration < 0 {
+		return fmt.Sprintf("Ошибка: отрицательная длительность (%d)", walkDuration)
+	}
+	distance := (float64(walkSteps) * stepLength) / float64(mInKm)
+	calories := WalkingSpentCalories(walkSteps, weight, height, walkDuration)
+	return fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила: %.2f км.\nВы сожгли: %.2f ккал.\n",
+		walkSteps, distance, calories)
 }
